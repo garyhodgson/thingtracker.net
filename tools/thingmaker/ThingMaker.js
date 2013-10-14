e8ce512c5065e3a71e504ad37551d48322d32021
Gary Hodgson <contact@garyhodgson.com>
2013-10-14 22:46:40 +0200

    update to thingmaker

--- tools/thingmaker/ThingMaker.js
+++ tools/thingmaker/ThingMaker.js
@@ -13,13 +13,13 @@ nodes = [["<host>", <port>], ["<host>", <port>], ...]
 nodes = [["127.0.0.1", 6881], ["your.router.node", 4804]]
  */
 
-// files to include
-var Files = null;
+var Files = null; // files to include
+var Directory = null; // directory (of multiple files)
 var Blobs = null; // collection of the file contents
-var PieceLength =  16384;
+var PieceLength =  16384; // chunk size for hash computation
 var Hashes; // ArrayBuffer of hashes
 
-function printpieces (pieces, tabspace)
+function printPieces (pieces, tabspace)
 {
     var view;
     var br;
@@ -43,9 +43,65 @@ function printpieces (pieces, tabspace)
     return (ret);
 }
 
+function printCertificate (cert, tabspace)
+{
+    var br;
+    var ret;
+
+    ret = "";
+
+    for (var j = 0; j < tabspace; j++)
+        ret += " ";
+    ret += "-----BEGIN CERTIFICATE-----\n";
+
+    var b64 = btoa (String.fromCharCode.apply (null, new Uint8Array (cert)));
+    br = 0;
+    for (var i = 0; i < b64.length; i++)
+    {
+        if (0 == (br % 64))
+            for (var j = 0; j < tabspace; j++)
+                ret += " ";
+        ret += b64.charAt (i);
+        if ((0 == (++br % 64)) && (i + 1 < b64.length))
+            ret += "\n";
+    }
+    ret += "\n";
+    for (var j = 0; j < tabspace; j++)
+        ret += " ";
+    ret += "-----END CERTIFICATE-----";
+
+    return (ret);
+}
+
+function printSignature (signature, tabspace)
+{
+    var view;
+    var br;
+    var ret;
+
+    ret = "";
+
+    view = new Uint8Array (signature);
+    br = 0;
+    for (var i = 0; i < view.byteLength; i++)
+    {
+        if (0 == (br % 32))
+            for (var j = 0; j < tabspace; j++)
+                ret += " ";
+        ret += ((view[i] >>> 4) & 0x0f).toString (16);
+        ret += (view[i] & 0x0f).toString (16);
+        if ((0 == (++br % 32)) && (i + 1 < view.byteLength))
+            ret += "\n";
+    }
+
+    return (ret);
+}
+
 function printTorrent (torrent)
 {
     var trigger = "\"pieces\": {}";
+    var cert = "\"certificate\": {}";
+    var signature = "\"signature\": {}";
     var index;
     var tabspace;
     var raw_text;
@@ -59,11 +115,37 @@ function printTorrent (torrent)
         tabspace = 0;
         while (" " == ret.substr (index - tabspace - 1, 1))
             tabspace++;
-        raw_text = printpieces (torrent["info"]["pieces"], tabspace + 4);
+        raw_text = printPieces (torrent["info"]["pieces"], tabspace + 4);
         s = "";
         for (var j = 0; j < tabspace; j++)
             s += " ";
         ret = ret.substr (0, index) + "\"pieces\": {\n" + raw_text + "\n" + s + "}" + ret.substr (index + trigger.length);
+    }
+    // kludgy way to show the certificate
+    index = ret.indexOf (cert);
+    if (0 < index)
+    {
+        tabspace = 0;
+        while (" " == ret.substr (index - tabspace - 1, 1))
+            tabspace++;
+        raw_text = printCertificate (torrent["signatures"]["net.thingtracker"]["certificate"], tabspace + 4);
+        s = "";
+        for (var j = 0; j < tabspace; j++)
+            s += " ";
+        ret = ret.substr (0, index) + "\"certificate\": {\n" + raw_text + "\n" + s + "}" + ret.substr (index + cert.length);
+    }
+    // kludgy way to show the signature
+    index = ret.indexOf (signature);
+    if (0 < index)
+    {
+        tabspace = 0;
+        while (" " == ret.substr (index - tabspace - 1, 1))
+            tabspace++;
+        raw_text = printSignature (torrent["signatures"]["net.thingtracker"]["signature"], tabspace + 4);
+        s = "";
+        for (var j = 0; j < tabspace; j++)
+            s += " ";
+        ret = ret.substr (0, index) + "\"signature\": {\n" + raw_text + "\n" + s + "}" + ret.substr (index + signature.length);
     }
 
     return (ret);
@@ -76,7 +158,20 @@ function ReadTorrent (torrent_file)
     // doesn't get converted to UTF-8 as required by the bencoding specification
     var binary_torrent = decode (torrent_file, true);
     var string_torrent = decode (torrent_file, false);
-    string_torrent["info"]["pieces"] = binary_torrent["info"]["pieces"];
+    if (binary_torrent["info"] && binary_torrent["info"]["pieces"])
+        string_torrent["info"]["pieces"] = binary_torrent["info"]["pieces"];
+    var signatures = string_torrent["signatures"];
+    if (signatures)
+        for (var identifier in signatures)
+            if (signatures.hasOwnProperty (identifier))
+            {
+                var cert = signatures[identifier]["certificate"];
+                if (cert)
+                    signatures[identifier]["certificate"] = binary_torrent["signatures"][identifier]["certificate"];
+                var sig = signatures[identifier]["signature"];
+                if (sig)
+                    signatures[identifier]["signature"] = binary_torrent["signatures"][identifier]["signature"];
+            }
     // add a utility function
     string_torrent.toString = function () { return (printTorrent (this)); };
 
@@ -86,6 +181,7 @@ function ReadTorrent (torrent_file)
 function ReadTorrentAsync (file, callback)
 {
     var reader = new FileReader ();
+    var name = file.name;
 
     // if we use onloadend, we need to check the readyState.
     reader.onloadend = function (evt)
@@ -95,7 +191,7 @@ function ReadTorrentAsync (file, callback)
             var torrent_file = evt.target.result;
             var torrent = ReadTorrent (torrent_file);
 
-            callback (torrent_file.name, torrent);
+            callback (name, torrent);
         }
     };
     reader.readAsArrayBuffer (file);
@@ -112,10 +208,16 @@ function str2ab (str)
     return (ret);
 }
 
-function ReadStringAsync (str, callback)
+function info_hash (info)
+{
+    return (sha1 (str2ab (encode (info))));
+}
+
+function ReadStringAsync (name, str, callback)
 {
     var arraybuffer = str2ab (str);
-    var blob = new Blob([arraybuffer], {type: "application/octet-binary"});
+    var blob = new Blob ([arraybuffer], {type: "application/octet-binary"});
+    blob.name = name;
     ReadTorrentAsync (blob, callback);
 }
 
@@ -217,7 +319,7 @@ function ReadFilesAsync (files, piece_length, callback)
             {
                 var d = 0;
                 for (var l = 0; l < 4; l++)
-                    d = (d << 8) + (temp[k + l] & 0xff);
+                    d = (d << 8) + (temp[(k * 4) + l] & 0xff);
                 raw_text += cvt_hex (d);
             }
             text += raw_text.toLowerCase () + "\n";
@@ -249,14 +351,14 @@ function ReadFilesAsync (files, piece_length, callback)
     }
 }
 
-function MakeTorrent ()
+function MakeTorrent (template)
 {
-    var epoch;
+    var timestamp;
     var infohash;
     var ret;
 
-    // javascript date is number of ms since epoch
-    epoch = Math.round ((new Date ()).getTime() / 1000.0);
+    // javascript date is number of millisconds since epoch
+    timestamp = Math.round ((new Date ()).getTime() / 1000.0);
     if (1 == Files.length)
         infohash = {
                 "length": Files[0].size,
@@ -269,24 +371,33 @@ function MakeTorrent ()
         filedata = [];
         for (var i = 0; i < Files.length; i++)
             filedata[filedata.length] = {
-                "length": Blobs[i].byteLength,
+                "length": Files[i].size,
                 "path" : [Files[i].name]
                 };
         infohash = {
                 "files": filedata,
-                "name": "SampleDirectory", // where to get the directory name?
+                "name": Directory,
                 "piece length": PieceLength,
                 "pieces": Hashes
             };
     }
-    ret =
+    if (null == template)
+        ret =
+        {
+            "created by": "ThingMaker v0.1",
+            "creation date": timestamp,
+            "encoding": "UTF-8",
+            "info": infohash
+        };
+    else
     {
-        //"comment": "Super simple thing.",
-        "created by": "ThingMaker v0.1",
-        "creation date": epoch,
-        "encoding": "UTF-8",
-        "info": infohash
-    };
+        ret = template;
+        var thing = ret["info"]["thing"]; // keep the thing data from the info section - if any
+        ret["creation date"] = timestamp;
+        ret["info"] = infohash;
+        if (null != thing)
+            ret["info"]["thing"] = thing;
+    }
 
     return (ret);
 }
